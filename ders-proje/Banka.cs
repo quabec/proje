b3f4360af1f841edc6a570285b3c03e980d7fe8d
Osman Mert ACAR <osmanmrtacar@yandex.com>
2017-12-14 18:32:52 +0300

    deneme

---
+++ ders-proje/Banka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_proje
+{
+    public class Banka
+    {
+        public List<Musteri> musteriler = new List<Musteri>();
+
+        public void MusteriEkle(Musteri m)
+        {
+            musteriler.Add(m);
+        }
+
+        public void MusteriSil(Musteri m)
+        {
+            musteriler.Remove(m);
+        }
+
+        public List<Musteri> Listele()
+        {
+            return musteriler;
+        }
+    }
+}
---
+++ ders-proje/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_proje
+{
+    public class Musteri
+    {
+        public int musteriNo { get; set; }
+        public string musteriAd { get; set; }
+        public string musteriSoyad { get; set; }
+
+    }
+}
